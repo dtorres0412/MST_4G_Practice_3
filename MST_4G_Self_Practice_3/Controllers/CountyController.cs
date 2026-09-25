8b53f3abc3d34bf9012229a58810f7b84a86bde4
Daniel Torres <C2601015@pic.net.tw>
2026-09-25 14:42:50 +0800

    (feat)_Added the CountyConttroller.cs file with the query and create Http API Attributes

---
+++ MST_4G_Self_Practice_3/Controllers/CountyController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using MST_4G_Self_Practice_3.Services;
+using MST_4G_Self_Practice_3.Dtos;
+
+namespace MST_4G_Self_Practice_3.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CountyController(ICountyService countyService) : ControllerBase
+{
+  [HttpGet("{countyNo}")]
+  public async Task<ActionResult<ReadCountyDto>>GetByCountyNo(string countyNo)
+    {
+        var County = await countyService.GetByCountyNoAsync(countyNo);
+
+        if (County == null)
+            return NotFound($"CountyNo record of '{countyNo}' not found.");
+
+        return Ok(County);
+    }
+  [HttpPost("create")]
+  public async Task<ActionResult<ReadCountyDto>>CreateCounty([FromBody] CreateCountyDto createCountyDto)
+    {
+        try
+        {
+            var result = await countyService.CreateCountyAsync(createCountyDto);
+
+            return CreatedAtAction(nameof(GetByCountyNo), new {countyNo = result?.CountyNo}, result);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }  
+}
