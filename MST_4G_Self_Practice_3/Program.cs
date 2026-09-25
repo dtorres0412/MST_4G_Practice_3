e94b47fd3096ba053299808de165963d7c5b899c
Daniel Torres <C2601015@pic.net.tw>
2026-09-25 14:49:47 +0800

    (fix)_Modified the Program.cs file by adding a scope for the county service files and adding the swaggerUI

--- MST_4G_Self_Practice_3/Program.cs
+++ MST_4G_Self_Practice_3/Program.cs
@@ -1,17 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MST_4G_Self_Practice_3.Data;
+using MST_4G_Self_Practice_3.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<ICountyService, CountyService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
