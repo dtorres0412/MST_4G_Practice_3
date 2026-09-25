010bf0faf167a4c6eb38ce24fe7cad5b2d90c5cd
Daniel Torres <C2601015@pic.net.tw>
2026-09-25 14:43:56 +0800

    (feat)_Added the Create and Read DTOs for County

---
+++ MST_4G_Self_Practice_3/Dtos/CreateCountyDto.cs
@@ -0,0 +1,10 @@
+namespace MST_4G_Self_Practice_3.Dtos;
+
+public class CreateCountyDto
+{
+
+    public string CountyNo { get; set; } = string.Empty;
+
+    public string CountyName { get ; set;} = string.Empty;
+    
+}
