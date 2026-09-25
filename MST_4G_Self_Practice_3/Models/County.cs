2324c2431993ec60fa0206fbe3f1b4a6f1faadd1
Daniel Torres <C2601015@pic.net.tw>
2026-09-25 14:45:02 +0800

    (feat)_Added County.cs model in the Models folder

---
+++ MST_4G_Self_Practice_3/Models/County.cs
@@ -0,0 +1,10 @@
+namespace MST_4G_Self_Practice_3.Models;
+
+public class County
+{
+    public int CountyId { get; set; }
+
+    public string CountyNo { get; set; } = string.Empty;
+
+    public string CountyName { get; set; } = string.Empty;
+}
